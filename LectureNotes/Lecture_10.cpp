f160d732338a1c33285d4bed274e314b843275d7
SeeWhySimon <137471535+SeeWhysimon@users.noreply.github.com>
2024-05-23 17:49:32 +0800

    Add files via upload

---
+++ LectureNotes/Lecture_10.cpp
@@ -0,0 +1,48 @@
+﻿#include "Lecture_10.h"
+
+int main() {
+	// Ex. 1
+	// std::string str = "Tsdafafsdaf";
+	// std::cout << count_occurrences(str.begin(), str.end(), 'f') << std::endl;
+
+	// Ex. 2
+	std::string str = "Xadia";
+	std::cout << count_occurrences(str.begin(), str.end(), isVowel) << std::endl;
+	// result : 3
+
+	// Ex. 3, using Lambda to count occurences
+	int limit = 5;
+	auto isMoreThan = [limit](int n) { return n > limit; };
+	std::vector<int> nums = { 3, 5, 6, 7, 9, 13 };
+
+	std::cout << count_occurrences(nums.begin(), nums.end(), isMoreThan) << std::endl;
+	// result : 4
+
+	// Ex. 4, using the functor
+	Adder<double> addDouble(5.9);
+	auto result = addDouble(4.12);
+	std::cout << result << std::endl;
+	// result : 10.02
+
+	// Ex. 5, using std::function
+	StandardFunction<int> = addInt;
+	std::cout << StandardFunction<int>(2, 3) << std::endl;
+	// result : 5
+	StandardFunction<int> = [](int a, int b) { return a + b; };
+	std::cout << StandardFunction<int>(2, 3) << std::endl;
+	// result : 5
+
+	// Virtual functions
+	Animal* animal = new Animal;
+	Dog* dog = new Dog;
+	AnimalFunc(animal);
+	// result : Animal.speak()
+	AnimalFunc(dog);
+	// result : Dog.speak()
+
+	result = std::count_if(nums.begin(), nums.end(), isMoreThan);
+	std::cout << result << std::endl;
+	// result : 4
+
+	return 0;
+}
---
+++ LectureNotes/Lecture_10.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+/*template <typename InputIt, typename DataType>
+int count_occurrences(InputIt begin, InputIt end, DataType target) {
+	int count = 0;
+	for (auto iter = begin; iter != end; iter++) {
+		if (*iter == target) count++;
+	}
+	return count;
+}
+*/
+
+template <typename InputIt, typename UniPred>
+int count_occurrences(InputIt begin, InputIt end, UniPred pred) {
+	int count = 0;
+	for (auto iter = begin; iter != end; iter++) {
+		if (pred(*iter)) count++;
+	}
+	return count;
+}
+
+bool isVowel(char c) {
+	std::string vowels = "aeiou";
+	return vowels.find(c) != std::string::npos;
+}
+
+bool isMoreThan(int num, int limit) {
+	return num > limit;
+}
+
+// a functor
+template <typename T>
+class Adder {
+public:
+	Adder(T value) : add_value(value) {}
+	T operator()(T num) { return num + add_value; }
+
+private:
+	T add_value;
+};
+
+template<typename Type>
+std::function<Type(Type, Type)> StandardFunction;
+
+int addInt(int a, int b) { return a + b; }
+
+struct intAdder {
+	int operator()(int a, int b) const {
+		return a + b;
+	}
+};
+
+class Animal {
+public:
+	virtual void speak() {
+		std::cout << "Animal.speak()" << std::endl;
+	}
+};
+
+class Dog : public Animal {
+public:
+	void speak() override {
+		std::cout << "Dog.speak()" << std::endl;
+	}
+};
+
+void AnimalFunc(Animal* animal) {
+	animal->speak();
+}
